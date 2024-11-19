f8fdc08cac817a2f87dab5ad1ebe12e5c024d248
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 17:32:11 +0900

    スキャフォールド - アプリバー - アプリバー

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,15 @@
 import 'package:flutter/material.dart';
-
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget { 
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  _mySnackBar(String msg){ // 1
-    return SnackBar(
-      content: Text('【スナックバー】$msg'),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final button = ElevatedButton( 
-      onPressed: () {
-        ScaffoldMessenger.of(context).showSnackBar(
-          _mySnackBar("メッセージ") // 2
-        );
-      },
-      child: const Text("ボタン"),
-    );
-    return Scaffold( body: button);
-  }
+  final appBar = AppBar( // 1
+    title: const Text('アプリのタイトル'), // 2 
+  );
+  final sc = Scaffold(
+    appBar: appBar,   // 3
+    body: const Text("テキスト"),
+  );
+  final app = MaterialApp(
+    debugShowCheckedModeBanner: false, // 4 
+    home: sc
+  );
+  runApp(app);
 }
