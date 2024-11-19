7dbff59e38944f869ffdfd19061b8545f71e9309
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 17:38:46 +0900

    スキャフォールド - ドロワー - リストビュー

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,31 @@
 import 'package:flutter/material.dart';
 void main() {  
-  final dc = DrawerHeader(child: Text('ドロワーのヘッダー')); // 1
+  final dc = ListView( // 1
+    children: [
+      DrawerHeader( // 2
+        padding: const EdgeInsets.all(0), // 3
+        child: Container( // 4
+          color: Colors.green[50],
+          alignment: Alignment.center,
+          child: const Text('ドロワーのヘッダー'),
+        ),
+      ),
+      ListTile( // 5
+        title: const Text('アイテム１'),
+        onTap: () { print('アイテム１'); },
+      ),
+      ListTile(
+        title: const Text('アイテム２'),
+        onTap: () { print('アイテム２'); },
+      ),
+      const Divider(), // 6
+      ListTile(
+        title: const Text('アイテム３'),
+        onTap: () { print('アイテム３'); },
+      ),
+    ],
+  );
+  
   final drawer = Drawer( // 2
     child: dc, // 3
   );
