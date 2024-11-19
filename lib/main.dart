7d72121a91c6be9355080f4e9806d83488527f54
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 17:34:56 +0900

    スキャフォールド - ポップアップメニュー - ポップアップメニュー

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
-void main() {
-  final appBar = AppBar( 
-    title: const Text('アプリのタイトル'),  
-    leading: IconButton( // ハンバーガーメニュー
-      icon: const Icon(Icons.menu), onPressed: () {},
-    ),
-    actions: [ 
-      IconButton( // ３点ドットメニュー
-        icon: const Icon(Icons.more_vert), onPressed: () {},
+void main() {  
+  final popupMenu = PopupMenuButton( // 1
+    icon: const Icon(Icons.more_vert), // 2
+    itemBuilder: (BuildContext context) => <PopupMenuEntry>[ // 3
+      const PopupMenuItem( // 4
+        value: 'search', // 5
+        child: ListTile( // 6
+          leading: Icon(Icons.search),
+          title : Text('検索する')
+        )
       ),
     ],
+    onSelected: (value){ // 7
+      switch(value){ // 8
+        case 'search': print("検索");
+      }
+    }  
   );
+
+  final appBar = AppBar(
+    title: const Text('アプリのタイトル'),
+    actions: [ popupMenu, ], // 9
+  );
+  
   final sc = Scaffold(
-    appBar: appBar, 
-    body: const Text("テキスト"),
-  );
+    appBar: appBar, body:
+    const Text("テキスト"), );
+  
   final app = MaterialApp(
-    debugShowCheckedModeBanner: false, 
+    debugShowCheckedModeBanner: false, // 10
     home: sc
   );
   runApp(app);
