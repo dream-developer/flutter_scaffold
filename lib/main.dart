eb0a190891e9d93632b052adc4db52d2d6d5e0d1
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 17:39:39 +0900

    スキャフォールド - ボトムナビゲーションバー - ボトムナビゲーションバー

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,81 @@
 import 'package:flutter/material.dart';
-void main() {  
-  final dc = ListView( // 1
-    children: [
-      DrawerHeader( // 2
-        padding: const EdgeInsets.all(0), // 3
-        child: Container( // 4
-          color: Colors.green[50],
-          alignment: Alignment.center,
-          child: const Text('ドロワーのヘッダー'),
-        ),
-      ),
-      ListTile( // 5
-        title: const Text('アイテム１'),
-        onTap: () { print('アイテム１'); },
-      ),
-      ListTile(
-        title: const Text('アイテム２'),
-        onTap: () { print('アイテム２'); },
-      ),
-      const Divider(), // 6
-      ListTile(
-        title: const Text('アイテム３'),
-        onTap: () { print('アイテム３'); },
-      ),
-    ],
-  );
-  
-  final drawer = Drawer( // 2
-    child: dc, // 3
-  );
-  final appBar = AppBar( // 4
-    title: const Text('アプリのタイトル'),
-    // leading: IconButton( // 5
-    //   icon: const Icon(Icons.info), onPressed: () {},
-    // ),
-  );
-  final sc = Scaffold(
-    appBar: appBar, // 6
-    drawer: drawer, // 7
-    body: const Text("テキスト"), // ボディー
-  );
 
-  final app = MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: sc
-  );
-  runApp(app);
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false, // デバッグラベルを消す
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget { 
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState(); 
+}
+
+class _MyHomePageState extends State<MyHomePage> { 
+  int _selectedIndex = 0; // 1
+
+  final _items = [ // 2
+    const BottomNavigationBarItem( // index:0
+      icon: Icon(Icons.home),
+      label: 'ホーム',
+      tooltip: '「ホーム」画面を表示します',
+    ),
+    const BottomNavigationBarItem( // index:1
+      icon: Icon(Icons.help),
+      label: '使い方',
+      tooltip: '「使い方」画面を表示します',
+    ),
+    const BottomNavigationBarItem( // index:2
+      icon: Icon(Icons.settings),
+      label: '設定',
+      tooltip: '「設定」画面を表示します',
+    ),
+  ];
+
+  final _screens = [ // 3
+    const Text('ホーム'),
+    const Text('使い方'),
+    const Text('設定'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final bnBar =  BottomNavigationBar( // 4
+      items: _items, // 5
+      backgroundColor: Colors.grey[800], // 6
+      selectedItemColor: Colors.white, // 7
+      unselectedItemColor: Colors.grey, // 8
+      currentIndex: _selectedIndex, // 9
+      onTap: (index) { // 10
+        setState(() {
+          _selectedIndex = index;
+        });
+      }
+    );
+
+    final body = Center(child: _screens[_selectedIndex]); // 11
+
+    return Scaffold(
+      body: body, // ボディー
+      bottomNavigationBar: bnBar, // ボトムナビゲーションバー
+    );
+  }
 }
