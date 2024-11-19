44f161ba522dfda6f48ae6d70e14bd7efb5e5061
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 17:32:49 +0900

    スキャフォールド - アプリバー - 配置

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 void main() {
   final appBar = AppBar( // 1
-    title: const Text('アプリのタイトル'), // 2 
+    backgroundColor: Colors.orange[50], // 2
+    title: const Text('アプリのタイトル'), // 3          
+    centerTitle: true, // 4
+    titleTextStyle: const TextStyle( // 5
+      fontStyle: FontStyle.italic,
+      fontSize: 20, 
+      color: Colors.green,
+    ),
+    leading: IconButton( // 6
+      icon: const Icon(Icons.info), onPressed: () {},
+    ),
+    actions: [ // 7
+      IconButton( icon: const Icon(Icons.share), onPressed: () {},),
+      IconButton( icon: const Icon(Icons.notifications), onPressed: () {},),
+    ],
   );
   final sc = Scaffold(
-    appBar: appBar,   // 3
+    appBar: appBar, // 8
     body: const Text("テキスト"),
   );
   final app = MaterialApp(
-    debugShowCheckedModeBanner: false, // 4 
+    debugShowCheckedModeBanner: false, // 9
     home: sc
   );
   runApp(app);
